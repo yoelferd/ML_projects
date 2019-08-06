4440fd45d42ebc666aa67776b2d02cc9a42fe5e1
Yoel Ferdman <yoel@minerva.kgi.edu>
2019-08-06 16:00:08 -0700

    Add files via upload

---
+++ Expectation Maximization and Gaussian Mixtures-estimating tanks in war/code.py
@@ -0,0 +1,103 @@
+
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import numpy as np
+# Generate some data
+from sklearn.datasets.samples_generator import make_blobs
+X, y_true = make_blobs(n_samples=400, centers=4,
+                       cluster_std=0.60, random_state=0)
+X = X[:, ::-1] # flip axes for better plotting
+# Plot the data with K Means Labels
+from sklearn.cluster import KMeans
+kmeans = KMeans(4, random_state=0)
+labels = kmeans.fit(X).predict(X)
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis');
+
+
+
+from sklearn import mixture
+
+def fit_samples(samples):
+	gmix = mixture.GMM(n_components=2, covariance_type='full')
+	gmix.fit(samples)
+	print gmix.means_
+	colors = ['r' if i==0 else 'g' for i in gmix.predict(samples)]
+	ax = plt.gca()
+	ax.scatter(samples[:,0], samples[:,1], c=colors, alpha=0.8)
+	plt.show()
+
+samples=[1,2,3,4]
+array.reshape(-1, 1)
+
+fit_samples(samples)
+'''
+
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
+
+def plot_kmeans(kmeans, X, n_clusters=4, rseed=0, ax=None):
+    labels = kmeans.fit_predict(X)
+
+    # plot the input data
+    ax = ax or plt.gca()
+    ax.axis('equal')
+    ax.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis', zorder=2)
+
+    # plot the representation of the KMeans model
+    centers = kmeans.cluster_centers_
+    radii = [cdist(X[labels == i], [center]).max()
+             for i, center in enumerate(centers)]
+    for c, r in zip(centers, radii):
+        ax.add_patch(plt.Circle(c, r, fc='#CCCCCC', lw=3, alpha=0.5, zorder=1))
+kmeans = KMeans(n_clusters=4, random_state=0)
+plot_kmeans(kmeans, X)
+rng = np.random.RandomState(13)
+X_stretched = np.dot(X, rng.randn(2, 2))
+
+kmeans = KMeans(n_clusters=4, random_state=0)
+plot_kmeans(kmeans, X_stretched)
+from sklearn.mixture import GMM
+gmm = GMM(n_components=4).fit(X)
+labels = gmm.predict(X)
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis');
+probs = gmm.predict_proba(X)
+print(probs[:5].round(3))
+size = 50 * probs.max(1) ** 2  # square emphasizes differences
+plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', s=size);
+from matplotlib.patches import Ellipse
+
+def draw_ellipse(position, covariance, ax=None, **kwargs):
+    """Draw an ellipse with a given position and covariance"""
+    ax = ax or plt.gca()
+    
+    # Convert covariance to principal axes
+    if covariance.shape == (2, 2):
+        U, s, Vt = np.linalg.svd(covariance)
+        angle = np.degrees(np.arctan2(U[1, 0], U[0, 0]))
+        width, height = 2 * np.sqrt(s)
+    else:
+        angle = 0
+        width, height = 2 * np.sqrt(covariance)
+    
+    # Draw the Ellipse
+    for nsig in range(1, 4):
+        ax.add_patch(Ellipse(position, nsig * width, nsig * height,
+                             angle, **kwargs))
+        
+def plot_gmm(gmm, X, label=True, ax=None):
+    ax = ax or plt.gca()
+    labels = gmm.fit(X).predict(X)
+    if label:
+        ax.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis', zorder=2)
+    else:
+        ax.scatter(X[:, 0], X[:, 1], s=40, zorder=2)
+    ax.axis('equal')
+    
+    w_factor = 0.2 / gmm.weights_.max()
+    for pos, covar, w in zip(gmm.means_, gmm.covars_, gmm.weights_):
+        draw_ellipse(pos, covar, alpha=w * w_factor)
+gmm = GMM(n_components=4, random_state=42)
+plot_gmm(gmm, X)
+gmm = GMM(n_components=4, covariance_type='full', random_state=42)
+plot_gmm(gmm, X_stretched)
+'''
